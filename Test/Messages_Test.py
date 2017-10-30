285f3760a5f15f07115d708418dd032708a59a7f
Shem <s.hale@aggiemail.usu.edu>
2017-10-29 20:37:03 -0600

    added more reply message unit tests

--- Test/Messages_Test.py
+++ Test/Messages_Test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 import unittest
+import numpy as np
 from datetime import datetime
 from CommunicationLibrary.Messages.AbstractMessages import *
 from CommunicationLibrary.Messages.ReplyMessages import *
@@ -52,6 +53,40 @@ class TestMessages(unittest.TestCase):
         self.assertEqual(process.status, 'idle')
         self.assertEqual(process.aliveTimeStamp, dateTime)
 
+    def testMotionDetectedReplyEncodingDecoding(self):
+        msg = MotionDetectedReply(True)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        self.assertEqual(msg.success, True)
+
+    def testRawQueryReplyEncodingDecoding(self):
+        picture = np.array([[0, 255], [255, 0]], np.uint8)
+        timeStamp = datetime.now()
+        data = PictureInfo(picture, timeStamp, 1, 2)
+        msg = RawQueryReply(True, data)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        self.assertEqual(msg.success, True)
+        data = processedMsg.data
+        self.assertEqual(data.picture, picture)
+        self.assertEqual(data.timeStamp, timeStamp)
+        self.assertEqual(data.cameraId, 1)
+        self.assertEqual(data.clusterId, 2)
+
+    def testRegisterReplyEncodingDecoding(self):
+        dateTime = datetime.now()
+        process = Process(2, 'Server', '127.0.0.3:3020', 'Statistics Server', 'busy', dateTime)
+        msg = RegisterReply(True, process)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        process = processedMsg.process
+        self.assertEqual(processedMsg.success, True)
+        self.assertEqual(process.processId, 2)
+        self.assertEqual(process.type, 'Server')
+        self.assertEqual(process.endPoint, '127.0.0.3:3020')
+        self.assertEqual(process.label, 'Statistics Server')
+        self.assertEqual(process.status, 'busy')
+        self.assertEqual(process.aliveTimeStamp, dateTime)
 
 if __name__ == '__main__':
     unittest.main()
