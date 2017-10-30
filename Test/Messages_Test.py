fa89337e3d1ac9315c807e8ed0989ad36380cb74
Shem <s.hale@aggiemail.usu.edu>
2017-10-29 20:14:18 -0600

    added more message unit tests

--- Test/Messages_Test.py
+++ Test/Messages_Test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python2
 import unittest
+from datetime import datetime
 from CommunicationLibrary.Messages.AbstractMessages import *
 from CommunicationLibrary.Messages.ReplyMessages import *
 from CommunicationLibrary.Messages.RequestMessages import *
+from CommunicationLibrary.Messages.SharedObjects import *
 
 
 class TestMessages(unittest.TestCase):
@@ -22,6 +24,33 @@ class TestMessages(unittest.TestCase):
         msg = Request()
         processedMsg = msg.encode().decode()
         self.assertEqual(msg, processedMsg)
+
+    def testAckReplyEncodingDecoding(self):
+        msg = AckReply(True)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        self.assertEqual(msg.success, True)
+
+    def testAliveReplyEncodingDecoding(self):
+        msg = AliveReply(True)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        self.assertEqual(msg.success, True)
+
+    def testLoginReplyEncodingDecoding(self):
+        dateTime = datetime.now()
+        process = Process(1, 'Server', '127.0.0.2:3000', 'Main Server', 'idle', dateTime)
+        msg = LoginReply(True, process)
+        processedMsg = msg.encode().decode()
+        self.assertEqual(msg, processedMsg)
+        process = processedMsg.process
+        self.assertEqual(processedMsg.success, True)
+        self.assertEqual(process.processId, 1)
+        self.assertEqual(process.type, 'Server')
+        self.assertEqual(process.endPoint, '127.0.0.2:3000')
+        self.assertEqual(process.label, 'Main Server')
+        self.assertEqual(process.status, 'idle')
+        self.assertEqual(process.aliveTimeStamp, dateTime)
 
 
 if __name__ == '__main__':
