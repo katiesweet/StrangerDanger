b4776d126f6893e93b8ffb1919c993d4debea269
Shem <s.hale@aggiemail.usu.edu>
2017-11-01 18:55:10 -0600

    added more message tests

--- Test/Messages_Test.py
+++ Test/Messages_Test.py
@@ -126,41 +126,97 @@ class TestMessages(unittest.TestCase):
         self.assertEqual(decodedMsg.process.label, 'Info about Process')
         self.assertEqual(decodedMsg.process.status, 'idle')
         self.assertEqual(decodedMsg.process.aliveTimeStamp, dateTime)
-    #
-    # def testLoginReplyEncodingDecoding(self):
-    #     dateTime = datetime.now()
-    #     process = Process(1, 'Server', '127.0.0.2:3000', 'Main Server', 'idle', dateTime)
-    #     msg = LoginReply(True, process)
-    #     processedMsg = msg.encode().decode()
-    #     self.assertEqual(msg, processedMsg)
-    #     process = processedMsg.process
-    #     self.assertEqual(processedMsg.success, True)
-    #     self.assertEqual(process.processId, 1)
-    #     self.assertEqual(process.type, 'Server')
-    #     self.assertEqual(process.endPoint, '127.0.0.2:3000')
-    #     self.assertEqual(process.label, 'Main Server')
-    #     self.assertEqual(process.status, 'idle')
-    #     self.assertEqual(process.aliveTimeStamp, dateTime)
-    #
-    # def testMotionDetectedReplyEncodingDecoding(self):
-    #     msg = MotionDetectedReply(True)
-    #     processedMsg = msg.encode().decode()
-    #     self.assertEqual(msg, processedMsg)
-    #     self.assertEqual(msg.success, True)
-    #
-    # def testRawQueryReplyEncodingDecoding(self):
-    #     picture = np.array([[0, 255], [255, 0]], np.uint8)
-    #     timeStamp = datetime.now()
-    #     data = PictureInfo(picture, timeStamp, 1, 2)
-    #     msg = RawQueryReply(True, data)
-    #     processedMsg = msg.encode().decode()
-    #     self.assertEqual(msg, processedMsg)
-    #     self.assertEqual(msg.success, True)
-    #     data = processedMsg.data
-    #     self.assertEqual(data.picture, picture)
-    #     self.assertEqual(data.timeStamp, timeStamp)
-    #     self.assertEqual(data.cameraId, 1)
-    #     self.assertEqual(data.clusterId, 2)
+
+    def testMotionDetectedReplyEncodingDecoding(self):
+        msg = MotionDetectedReply(True)
+        self.assertIsNot(msg, None)
+        self.assertEqual(msg.success, True)
+        msg.initConversationIdMessageId(2,3)
+        self.assertEqual(msg.conversationId, 2)
+        self.assertEqual(msg.messageId, 3)
+        encodedMsg = msg.encode()
+        decodedMsg = Message.decode(encodedMsg)
+        self.assertIsNot(decodedMsg, None)
+        self.assertTrue(isinstance(decodedMsg, Message))
+        self.assertTrue(isinstance(decodedMsg, Reply))
+        self.assertTrue(isinstance(decodedMsg, MotionDetectedReply))
+        self.assertEqual(decodedMsg.conversationId, 2)
+        self.assertEqual(decodedMsg.messageId, 3)
+        self.assertEqual(decodedMsg.success, True)
+
+    def testRawQueryReplyEncodingDecoding(self):
+        picture = np.array([[0, 255], [255, 0]], np.uint8)
+        timeStamp = datetime.now()
+        data = PictureInfo(picture, timeStamp, 1, 2)
+        msg = RawQueryReply(True, data)
+        self.assertIsNot(msg, None)
+        self.assertEqual(msg.success, True)
+
+        self.assertIsNot(msg.data, None)
+        self.assertTrue(np.array_equal(msg.data.picture, np.array([[0, 255], [255, 0]], np.uint8)))
+        self.assertEqual(msg.data.timeStamp, timeStamp)
+        self.assertEqual(msg.data.cameraId, 1)
+        self.assertEqual(msg.data.clusterId, 2)
+
+        msg.initConversationIdMessageId(2,3)
+        self.assertEqual(msg.conversationId, 2)
+        self.assertEqual(msg.messageId, 3)
+
+        encodedMsg = msg.encode()
+        decodedMsg = Message.decode(encodedMsg)
+        self.assertIsNot(decodedMsg, None)
+
+        self.assertTrue(isinstance(decodedMsg, Message))
+        self.assertTrue(isinstance(decodedMsg, Reply))
+        self.assertTrue(isinstance(decodedMsg, RawQueryReply))
+
+        self.assertEqual(decodedMsg.conversationId, 2)
+        self.assertEqual(decodedMsg.messageId, 3)
+        self.assertEqual(decodedMsg.success, True)
+
+        self.assertIsNot(decodedMsg.data, None)
+        self.assertTrue(np.array_equal(decodedMsg.data.picture, np.array([[0, 255], [255, 0]], np.uint8)))
+        self.assertEqual(decodedMsg.data.timeStamp, timeStamp)
+        self.assertEqual(decodedMsg.data.cameraId, 1)
+        self.assertEqual(decodedMsg.data.clusterId, 2)
+
+    def testRegisterReplyEncodingDecoding(self):
+        dateTime = datetime.now()
+        process = ProcessInfo(1, 'ClientProcess', '127.0.0.3:3200', 'Info about Process', 'idle', dateTime)
+        msg = RegisterReply(True, process)
+        self.assertIsNot(msg, None)
+        self.assertEqual(msg.success, True)
+        self.assertIsNot(msg.process, None)
+        self.assertEqual(msg.process.processId, 1)
+        self.assertEqual(msg.process.processType, 'ClientProcess')
+        self.assertEqual(msg.process.endPoint, '127.0.0.3:3200')
+        self.assertEqual(msg.process.label, 'Info about Process')
+        self.assertEqual(msg.process.status, 'idle')
+        self.assertEqual(msg.process.aliveTimeStamp, dateTime)
+
+        msg.initConversationIdMessageId(2,3)
+        self.assertEqual(msg.conversationId, 2)
+        self.assertEqual(msg.messageId, 3)
+
+        encodedMsg = msg.encode()
+        decodedMsg = Message.decode(encodedMsg)
+        self.assertIsNot(decodedMsg, None)
+
+        self.assertTrue(isinstance(decodedMsg, Message))
+        self.assertTrue(isinstance(decodedMsg, Reply))
+        self.assertTrue(isinstance(decodedMsg, RegisterReply))
+
+        self.assertEqual(decodedMsg.conversationId, 2)
+        self.assertEqual(decodedMsg.messageId, 3)
+        self.assertEqual(decodedMsg.success, True)
+
+        self.assertIsNot(decodedMsg.process, None)
+        self.assertEqual(decodedMsg.process.processId, 1)
+        self.assertEqual(decodedMsg.process.processType, 'ClientProcess')
+        self.assertEqual(decodedMsg.process.endPoint, '127.0.0.3:3200')
+        self.assertEqual(decodedMsg.process.label, 'Info about Process')
+        self.assertEqual(decodedMsg.process.status, 'idle')
+        self.assertEqual(decodedMsg.process.aliveTimeStamp, dateTime)
     #
     # def testRegisterReplyEncodingDecoding(self):
     #     dateTime = datetime.now()
