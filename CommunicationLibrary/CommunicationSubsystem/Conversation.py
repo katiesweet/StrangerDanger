43bba6263544a5622f04de4b05f82bd26f125bd4
Sarah Isaac <sarahisaac31@gmail.com>
2017-11-06 07:18:22 -0700

    updated base convo

--- CommunicationLibrary/CommunicationSubsystem/Conversation.py
+++ CommunicationLibrary/CommunicationSubsystem/Conversation.py
@@ -14,24 +14,24 @@ class BaseConversation(object):
         self.toSocketQueue = toSocketQueue # Used to send a message
         self.fromConversationQueue = fromConversationQueue # Used to pass a message to the main application
 
-        self.myOutgoingMessageQueue = Queue()
-        self.myIncomingMessageQueue = Queue()
+        self.myOutgoingMessageQueue = Queue.Queue()
+        self.myIncomingMessageQueue = Queue.Queue()
 
         if envelopeIsOutgoing:
-            self.myOutgoingMessageQueue.append(envelope)
+            self.myOutgoingMessageQueue.put(envelope)
         else:
-            self.myIncomingMessageQueue.append(envelope)
+            self.myIncomingMessageQueue.put(envelope)
 
         self.shouldRun = True
         thread.startNewThread(self.__run)
 
     def sendNewMessage(self, envelope):
         """Called from conversation manager for when the application wishes to send a message as a part of the conversation. """
-        self.myOutgoingMessageQueue.append(envelope)
+        self.myOutgoingMessageQueue.put(envelope)
 
     def receivedNewMessage(self, envelope):
         """Called from conversation manager for when a socket receives a message intended for this conversation. """
-        self.myIncomingMessageQueue.append(envelope)
+        self.myIncomingMessageQueue.put(envelope)
 
     def __run(self):
         while self.shouldRun:
