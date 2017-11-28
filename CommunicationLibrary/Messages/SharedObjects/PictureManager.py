ca3508e124852f2bf8e08549cd0c5d98c77e95d4
Sarah Isaac <sarahisaac31@gmail.com>
2017-11-28 10:17:30 -0700

    uncommented code

--- CommunicationLibrary/Messages/SharedObjects/PictureManager.py
+++ CommunicationLibrary/Messages/SharedObjects/PictureManager.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 import math
-# import numpy as np
+import numpy as np
 
 class PictureManager:
 
     @staticmethod
     def splitPicture(picture, sizeOfParts):
-        # rows = picture.shape[0]
-        # numberOfParts = math.ceil(rows/sizeOfParts)
-        # splitFrames = np.array_split(picture, numberOfParts)
-        # return splitFrames, numberOfParts
-        return [1,2,3], 5
+        rows = picture.shape[0]
+        numberOfParts = math.ceil(rows/sizeOfParts)
+        splitFrames = np.array_split(picture, numberOfParts)
+        return splitFrames, numberOfParts
 
     @staticmethod
     def combinePicture(splitFrames):
-        # return np.concatenate(splitFrames)
-        return [1,2,3,4,5]
+        return np.concatenate(splitFrames)
