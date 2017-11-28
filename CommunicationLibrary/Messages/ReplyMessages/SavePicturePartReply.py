54c6cf2fa991ce70f9f17a48bb5af41b1d54b361
Shem <s.hale@aggiemail.usu.edu>
2017-11-27 19:32:49 -0700

    added picture related messages

--- CommunicationLibrary/Messages/ReplyMessages/SavePicturePartReply.py
+++ CommunicationLibrary/Messages/ReplyMessages/SavePicturePartReply.py
@@ -3,7 +3,6 @@ from CommunicationLibrary.Messages.AbstractMessages.Reply import Reply
 
 class SavePicturePartReply(Reply):
 
-    def __init__(self, success, partNumber, cameraName):
+    def __init__(self, success, partNumber):
         super(SavePicturePartReply, self).__init__(success)
         self.partNumber = partNumber
-        self.cameraName = cameraName
