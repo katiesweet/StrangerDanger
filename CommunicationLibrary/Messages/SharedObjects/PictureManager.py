db96277be47423a20210e8ab4be6b91f4d740d4f
Sarah Isaac <sarahisaac31@gmail.com>
2017-12-02 17:26:34 -0700

    conversation from client to main server for single raw data of pic

--- CommunicationLibrary/Messages/SharedObjects/PictureManager.py
+++ CommunicationLibrary/Messages/SharedObjects/PictureManager.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 import math
-import numpy as np
+# import numpy as np
 
 class PictureManager:
 
     @staticmethod
     def splitPicture(picture, sizeOfParts):
-        rows = picture.shape[0]
-        numberOfParts = math.ceil(rows/sizeOfParts)
-        splitFrames = np.array_split(picture, numberOfParts)
-        return splitFrames, numberOfParts
+        # rows = picture.shape[0]
+        # numberOfParts = math.ceil(rows/sizeOfParts)
+        # splitFrames = np.array_split(picture, numberOfParts)
+        # return splitFrames, numberOfParts
+        return [1,2,3,4,5], 5
 
     @staticmethod
     def combinePicture(splitFrames):
-        return np.concatenate(splitFrames)
+        # return np.concatenate(splitFrames)
+        return 'hi'
